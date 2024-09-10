65ccbffc54056106a52d0cc5bc68c0c34f297bef
mattesmi <127109845+st10314794@users.noreply.github.com>
2024-09-10 19:47:59 +0200

    Fixed toastr notifcation not showing up after submitting

--- TaskOneDraft/obj/Debug/net8.0/TaskOneDraft.AssemblyInfo.cs
+++ TaskOneDraft/obj/Debug/net8.0/TaskOneDraft.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskOneDraft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9a900aad1905fe6065e14cc52c29089f86a45a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8132d4bdb58f8a13a2d3b25d905f1d88b44afa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskOneDraft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskOneDraft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
